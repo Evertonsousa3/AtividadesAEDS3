ea1e0bcabb97dfbe2efc3e12d69f8cca8589a75e
Jônatas Pereira <jonataspereirarocha@gmail.com>
2023-05-26 16:46:46 -0300

    Create fIlaPrioridade.py

---
+++ fIlaPrioridade.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, valor):
+        self.valor = valor
+        self.proximo = None
+
+
+class FilaPrioridade:
+    def __init__(self):
+        self.inicio = None
+        self.final = None
